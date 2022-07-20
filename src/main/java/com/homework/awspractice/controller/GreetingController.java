b35009e02676813a22fb650a8b97c5c2a2a8230a
christian0412-perez <193256@ids.puchiapas.edu.mx>
2022-07-20 00:13:08 -0500

    aaa

--- src/main/java/com/homework/awspractice/controller/GreetingController.java
+++ src/main/java/com/homework/awspractice/controller/GreetingController.java
@@ -1,20 +1,14 @@
 package com.homework.awspractice.controller;
 
-import com.homework.awspractice.model.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, I'm alive. Nice to meet you %s!";
-    private final AtomicLong counter = new AtomicLong();
-
-    @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = ":D") String name){
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    @GetMapping("/saludar")
+    public String saludo(){
+        return "hola jeje";
     }
 }
